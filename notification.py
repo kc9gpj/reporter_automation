b590c5e461155c85705b0cb28efb90abf0194112
David Hoffmann <dave@dhoff.net>
2022-03-20 22:41:56 -0500

    add notification.py

--- notification.py
+++ notification.py
@@ -1,15 +1,13 @@
 import requests
 
-ignore_list = ['k9yv','w0gmv','wb0zan','wd0emr','wq0p','k0im']
+ignore_list = ['k9yv','w0gmv','wb0zan','wd0emr','wq0p','k0im','n1ccc']
 file1 = open('ALL.TXT', 'r')
 Lines = file1.readlines()
  
 for line in Lines:
     ignore = any(n in line.lower() for n in ignore_list) 
     if not ignore:
-        print("Line{}: {}".format(count, line.strip()))
-        url = "https://api.simplepush.io/send/Ere7WA/{}".format(line)
-        response = requests.request("GET", url)
-        print(response.text)
+        payload = {'message': line, 'user': '', 'token': '' }
+        r = requests.post('https://api.pushover.net/1/messages.json', data=payload, headers={'User-Agent': 'Python'})
         open('ALL.TXT', 'w')
         break
